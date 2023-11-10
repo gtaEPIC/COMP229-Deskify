10f10a1b01a28b6068013509efc69251be5c32cd
gtaEPIC <johnnygotrain@gmail.com>
2023-11-10 03:00:10 -0500

    Added the Create, Get, and Update routes

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -1,11 +1,69 @@
 const ticketModel = require('../models/ticket');
 
+// TODO: Add proper authentication to all routes
+
+// List all tickets
 module.exports.list = async function (req, res, next) {
     try {
         let list = await ticketModel.find({}); // .populate('User');
-        res.json(list);
+        res.json({ success: true, list: list });
     } catch (error) {
         console.log(error);
         next(error);
+    }
+}
+
+// Get a ticket by record
+module.exports.getTicketByRecord = async function (req, res, next) {
+    try {
+        let ticket = await ticketModel.findOne({ record: req.params.id });
+        if (!ticket)
+            throw new Error('Ticket not found. Are you sure it exists?')
+
+        res.json({ success: true, ticket: ticket });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+}
+
+// Create a ticket
+module.exports.createTicket = async function (req, res, next) {
+    try {
+        let ticket = await ticketModel.create(req.body);
+        res.json({ success: true, ticket: ticket });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+}
+
+// Update a ticket
+module.exports.updateTicket = async function (req, res, next) {
+    try {
+        let ticket = await ticketModel.findOne({ record: req.params.id });
+        if (!ticket)
+            throw new Error('Ticket not found. Are you sure it exists?')
+
+        let updatedTicket = ticketModel(req.body);
+        updatedTicket._id = ticket._id;
+
+        let result = await ticketModel.updateOne({ _id: ticket._id }, updatedTicket);
+        console.log(result);
+        if (result.modifiedCount > 0) {
+            res.json(
+                {
+                    success: true,
+                    message: "Ticket updated successfully."
+                }
+            );
+        }
+        else {
+            // Express will catch this on its own.
+            throw new Error('Ticket not updated. Are you sure it exists?')
+        }
+    } catch (error) {
+        console.log(error);
+        next(error)
     }
 }
--- routes/ticketRoute.js
+++ routes/ticketRoute.js
@@ -5,5 +5,11 @@ const ticketController = require('../controllers/ticketController');
 
 // Get all tickets
 router.get('/', ticketController.list);
+// Get a ticket by record
+router.get('/get/:id', ticketController.getTicketByRecord);
+// Create a ticket
+router.post('/', ticketController.createTicket);
+// Update a ticket
+router.put('/:id', ticketController.updateTicket);
 
 module.exports = router;
