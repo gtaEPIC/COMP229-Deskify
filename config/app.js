8ebc7db6e58c17f3be854fa1de9f5025880262d2
Johnathan Hall <32531985+gtaEPIC@users.noreply.github.com>
2023-11-11 22:26:17 -0500

    Update config/app.js

--- config/app.js
+++ config/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/ticket', ticketRouter);
-app.use('/userRegistration', userRegistrationRouter);
+app.use('/users', userRegistrationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
