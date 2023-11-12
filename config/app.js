6bb7c20cd45d46b997f66dd9dfe150551374b6db
gtaEPIC <johnnygotrain@gmail.com>
2023-11-12 00:28:36 -0500

    Fixed the issues

--- config/app.js
+++ config/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', indexRouter);
 app.use('/ticket', ticketRouter);
-app.use('/users', userRegistrationRouter);
+app.use('/user', userRegistrationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
