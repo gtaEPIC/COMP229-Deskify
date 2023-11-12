a0b3c3d0b7124a22de7b0b3fac7bc718b4be263a
imirza7 <127257130+imirza7@users.noreply.github.com>
2023-11-11 22:24:44 -0500

    Delete index.js

--- controllers/index.js
+++
@@ -1,6 +0,0 @@
-module.exports.home = function (req, res, next) {
-    res.send("Welcome to Deskify");
-}
-module.exports.userRegistration = function (req, res, next) {
-    res.send("Welcome to User Registration");
-}
