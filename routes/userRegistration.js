dce84d9d9131097fc1e5e9fd74f290d1e2bdc909
Johnathan Hall <32531985+gtaEPIC@users.noreply.github.com>
2023-11-11 22:26:23 -0500

    Update routes/userRegistration.js

--- routes/userRegistration.js
+++ routes/userRegistration.js
@@ -3,10 +3,10 @@ var router = express.Router();
 
 let userRegistrationController = require("../controllers/userResgistration");
 
-router.get("/users", userRegistrationController.getAllUsers);
-router.post("/create", userRegistrationController.createUser);
-router.get("/get/:username", userRegistrationController.userByusername);
-router.put("/edit/:username", userRegistrationController.update);
-router.delete("/delete/:username", userRegistrationController.deleteUser);
+router.get("/", userRegistrationController.getAllUsers);
+router.post("/", userRegistrationController.createUser);
+router.get("/:username", userRegistrationController.userByusername);
+router.put("/:username", userRegistrationController.update);
+router.delete("/:username", userRegistrationController.deleteUser);
 
 module.exports = router;
