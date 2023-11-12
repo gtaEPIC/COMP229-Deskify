7bcabc08b018858d4b8663af272e0420bd717911
gtaEPIC <johnnygotrain@gmail.com>
2023-11-11 19:26:08 -0500

    Made the view ticket page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+let ticketModel = require('../models/ticket');
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
 // });
+
+router.get('/tickets/:id', async function(req, res, next) {
+    let record = req.params.id;
+    let ticket = await ticketModel.findOne({ record: req.params.id });
+    if (!ticket) throw new Error('Ticket not found. Are you sure it exists?')
+    res.render('ticketView', { title: 'Ticket', ticket: ticket });
+});
 
 module.exports = router;
