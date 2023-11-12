5d8c655f15ad24dc882dab6736c7c1389d2e2566
Johnathan Hall <32531985+gtaEPIC@users.noreply.github.com>
2023-11-11 22:26:32 -0500

    Update routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,4 @@ var router = express.Router();
 
 let indexController = require('../controllers');
 
-router.get('/', indexController.home);
-router.get('/userRegistration', indexController.userRegistration);
 module.exports = router;
